9475f2568a33de165022ca4251aeb6c07ecc176d
aoisupersix <aoi_supersix@yahoo.co.jp>
2018-04-29 11:04:31 +0900

    [add] ステータスの手動更新を実装

--- HLBeacon/ViewController/MainViewController.swift
+++ HLBeacon/ViewController/MainViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import UserNotifications
+import RealmSwift
 
 class MainViewController: UIViewController {
     private var hLabUsers: [HLabUserData] = []
@@ -16,6 +17,8 @@ class MainViewController: UIViewController {
     
     @IBOutlet var statusLabel: UILabel!
     @IBOutlet var rssiLabel: UILabel!
+    @IBOutlet var slackIdLabel: UILabel!
+    @IBOutlet var hLabIdentifierLabel: UILabel!
     
     var uiUpdateTimer: Timer? = nil
     
@@ -90,6 +93,50 @@ class MainViewController: UIViewController {
             }.resume()
     }
     
+    /// ステータス情報をHLManagerにプッシュします
+    /// - parameter status: 0であれば外出,1であれば在室
+    private func sendStatus(status: Int) {
+        let userData = RealmUserDataManager().getData()
+        if userData.slackUserId == "-1" || userData.hId == "-1" {
+            //ユーザ情報不足
+            return
+        }
+        let url = URL(string: "https://script.google.com/macros/s/AKfycbwsB4Tvdfk50byMeg2Yghp4lkyqaxgWsdH2pI1KkP81eLAYfwQ/exec")
+        var request = URLRequest(url: url!)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        let params: [[String: String]] = [[
+            "id": userData.hId,
+            "status": status.description
+            ]]
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: params, options: .prettyPrinted)
+            
+            URLSession.shared.dataTask(with: request) { data, response, err in
+                if err != nil {
+                    print("SLACK-USERS-GET: Failed")
+                    return;
+                }
+                print("SLACK-USERS-GET: Success")
+                print(data!)
+                }.resume()
+        }catch{
+            fatalError(error.localizedDescription)
+        }
+    }
+    
+    /// 確認アラートを表示します。
+    /// - parameter title: アラートのタイトル
+    /// - parameter message: アラートのメッセージ
+    private func pushConfirmAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        let defaultAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: { (action: UIAlertAction!) in
+            self.dismiss(animated: true, completion: nil)
+        })
+        alert.addAction(defaultAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         getHLabUsers()
@@ -99,7 +146,10 @@ class MainViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        //Viewの再描画 TODO
+        //Viewの再描画
+        let userData = RealmUserDataManager().getData()
+        slackIdLabel.text = "SlackId: \(userData.slackUserId)"
+        hLabIdentifierLabel.text = "HLabIdentifier: \(userData.hIdentifier)"
     }
     
     override func didReceiveMemoryWarning() {
@@ -119,6 +169,18 @@ class MainViewController: UIViewController {
         }
     }
     
+    /// 外出ステータスをHLabManagerにプッシュします
+    @IBAction func setStatusToOutRoom(_ sender: Any) {
+        sendStatus(status: 0)
+        pushConfirmAlert(title: "ステータス変更完了", message: "HLabManagerのステータスを外出に変更しました。")
+    }
+    
+    /// 在室ステータスをHLabManagerにプッシュします
+    @IBAction func setStatusToInRoom(_ sender: Any) {
+        sendStatus(status: 1)
+        pushConfirmAlert(title: "ステータス変更完了", message: "HLabManagerのステータスを在室に変更しました。")
+    }
+    
     ///UserNameInputViewに遷移します。
     @IBAction func PerformUsernameInputView(_ sender: Any) {
         if isCompleteSlackConnection {
@@ -134,6 +196,7 @@ class MainViewController: UIViewController {
         }
     }
     
+    ///UIのラベルを更新します
     @objc func updateStatus() {
         if LocationManager.isEnterRegion {
             statusLabel.text = "在室"
